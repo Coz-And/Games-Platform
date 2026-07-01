f1a1dbd816b59be57bdd9ad60867ffe2e6cb20b6
Andrea Cozzi <165024778+Coz-And@users.noreply.github.com>
2026-07-01 19:15:06 +0200

    7.1

--- central-server/src/main/java/com/connectedgames/config/SecurityConfig.java
+++ central-server/src/main/java/com/connectedgames/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/",
+                                "/index",
                                 "/login",
                                 "/registrazione",
                                 "/giochi",
