4bcefedd5c2a651e66bf3b5f4549556ce1637fc6
Andrea Cozzi <165024778+Coz-And@users.noreply.github.com>
2026-07-03 15:16:22 +0200

    3.4

--- central-server/src/main/java/com/connectedgames/repository/RisultatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.connectedgames.repository;
-
-import com.connectedgames.model.Risultato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RisultatoRepository extends JpaRepository<Risultato, Long> {
-}
--- edge-server/src/main/java/com/connectedgames/sync/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.connectedgames.sync;
-
-import com.connectedgames.model.EventoSensore;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EventoRepository extends JpaRepository<EventoSensore, Long> {
-    List<EventoSensore> findBySincronizzatoFalse();
-}
